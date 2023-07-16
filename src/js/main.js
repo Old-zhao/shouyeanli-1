17e91f94d338fbf3771b929ebebdb6bc4d8dc929
Old_zhao <937500081@qq.com>
2023-07-16 21:54:44 +0800

    banner 2/1

---
+++ src/js/main.js
@@ -0,0 +1,34 @@
+
+console.log(jQuery)
+
+
+let previous = jQuery('#previous')
+let next = $("#next")
+let banner = $(".banner")
+console.log(banner)
+
+getComputedStyle(div,null)
+
+
+
+
+
+// previous.click(function(){
+//     for(let i=0; i<img_array.length; i++){
+
+//     }
+// })
+
+next.click(function(){
+    alert('被点击了')
+})
+
+let img_array = [
+    "../src/images/slide.jpg",
+    "../src/images/slide1.jpg",
+    "../src/images/slide2.jpg"
+]
+
+function change(count){
+
+}
